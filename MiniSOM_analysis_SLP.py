e6dba76ae23a9e8a1c9f38979f48116e5dbbae75
Sawyer Brand <40899724+SawyerBrand@users.noreply.github.com>
2024-05-10 10:46:16 -0600

    Create MiniSOM_analysis_SLP.py

---
+++ MiniSOM_analysis_SLP.py
@@ -0,0 +1,28 @@
+import numpy as np
+import python as pd
+import import numpy as np
+import pandas as pd
+import minisom
+import xarray as xr
+from minisom import MiniSom
+import matplotlib.pyplot as plt
+
+# load libraries
+import xarray as xr
+from minisom import MiniSom
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.gridspec import GridSpec
+from sklearn.preprocessing import MinMaxScaler, scale
+import sklearn.preprocessing
+import numpy as np
+
+import cartopy.crs as ccrs
+
+import cartopy.feature as cfeature
+
+########
+
+# historical 0101: net OHT into Arctic through following gateways:
+# chosen sections = bering, davis, denmark, hudson bay, iceland-faroe-scotland
+~                                                                                
